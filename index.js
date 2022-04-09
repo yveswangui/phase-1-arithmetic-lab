da0cef22173067d3331f57b3ceebfde19477931f
Yvonne <yveswangui@gmail.com>
2022-04-10 00:02:56 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10)
+
+const num3 = 34;
+const num4 = 5;
+const mod = num3 % num4
+
+const max = Math.max(1,2,17,4,20,6,14,2);
+
+
